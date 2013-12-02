d93e107a2a1f0a51eeacdcce33b2ad3e0b8eda6b
yelo <zhihuzeye@gmail.com>
2013-12-03 00:01:00 +0800

    add clipboard plugin for debug

---
+++ libs/phonegap/clipboard.js
@@ -0,0 +1,30 @@
+var cordova = require('cordova');
+
+/**
+ * Clipboard plugin for Cordova
+ * 
+ * @constructor
+ */
+function Clipboard () {}
+
+/**
+ * Sets the clipboard content
+ *
+ * @param {String} text The content to copy to the clipboard
+ */
+Clipboard.prototype.copy = function (text, onSuccess, onFail) {
+        cordova.exec(onSuccess, onFail, "Clipboard", "copy", [text]);
+}
+
+/**
+ * Gets the clipboard content
+ *
+ * @param {Function} callback The function to call with the result
+ */
+Clipboard.prototype.paste = function (onSuccess, onFail) {
+        cordova.exec(onSuccess, onFail, "Clipboard", "paste", []);
+}
+
+// Register the plugin
+var clipboard = new Clipboard();
+module.exports = clipboard;
--- remainder.md
+++
@@ -1,3 +0,0 @@
-ios测试
-testtest
-
