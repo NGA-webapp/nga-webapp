bca92ae48e717fab42c14cf8b8294416fd520b29
yelo <zhihuzeye@gmail.com>
2014-01-20 00:54:56 +0800

    add emtion datas

---
+++ js/config/emotion.js
@@ -0,0 +1,378 @@
+define(function (require, exports, module) {
+  module.exports = {
+    defaults: {
+      name: '默认',
+      path: 'defaults',
+      items: 
+        [
+          {
+            url: 'http://img4.ngacn.cc/ngabbs/post/smile/smile.gif',
+            filename: 'smile.gif',
+            code: '[s:0:1]'
+          },
+          {
+            url: 'http://img4.ngacn.cc/ngabbs/post/smile/mrgreen.gif',
+            filename: 'mrgreen.gif',
+            code: '[s:0:2]'
+          },
+          {
+            url: 'http://img4.ngacn.cc/ngabbs/post/smile/question.gif',
+            filename: 'question.gif',
+            code: '[s:0:3]'
+          },
+          {
+            url: 'http://img4.ngacn.cc/ngabbs/post/smile/wink.gif',
+            filename: 'wink.gif',
+            code: '[s:0:4]'
+          },
+          {
+            url: 'http://img4.ngacn.cc/ngabbs/post/smile/redface.gif',
+            filename: 'redface.gif',
+            code: '[s:0:5]'
+          },
+          {
+            url: 'http://img4.ngacn.cc/ngabbs/post/smile/sad.gif',
+            filename: 'sad.gif',
+            code: '[s:0:6]'
+          },
+          {
+            url: 'http://img4.ngacn.cc/ngabbs/post/smile/cool.gif',
+            filename: 'cool.gif',
+            code: '[s:0:7]'
+          },
+          {
+            url: 'http://img4.ngacn.cc/ngabbs/post/smile/crazy.gif',
+            filename: 'crazy.gif',
+            code: '[s:0:8]'
+          },
+          {
+            url: 'http://img4.ngacn.cc/ngabbs/post/smile/12.gif',
+            filename: '12.gif',
+            code: '[s:0:32]'
+          },
+          {
+            url: 'http://img4.ngacn.cc/ngabbs/post/smile/13.gif',
+            filename: '13.gif',
+            code: '[s:0:33]'
+          },
+          {
+            url: 'http://img4.ngacn.cc/ngabbs/post/smile/14.gif',
+            filename: '14.gif',
+            code: '[s:0:34]'
+          },
+          {
+            url: 'http://img4.ngacn.cc/ngabbs/post/smile/10.gif',
+            filename: '10.gif',
+            code: '[s:0:30]'
+          },
+          {
+            url: 'http://img4.ngacn.cc/ngabbs/post/smile/09.gif',
+            filename: '09.gif',
+            code: '[s:0:29]'
+          },
+          {
+            url: 'http://img4.ngacn.cc/ngabbs/post/smile/08.gif',
+            filename: '08.gif',
+            code: '[s:0:28]'
+          },
+          {
+            url: 'http://img4.ngacn.cc/ngabbs/post/smile/07.gif',
+            filename: '07.gif',
+            code: '[s:0:27]'
+          },
+          {
+            url: 'http://img4.ngacn.cc/ngabbs/post/smile/06.gif',
+            filename: '06.gif',
+            code: '[s:0:26]'
+          },
+          {
+            url: 'http://img4.ngacn.cc/ngabbs/post/smile/05.gif',
+            filename: '05.gif',
+            code: '[s:0:25]'
+          },
+          {
+            url: 'http://img4.ngacn.cc/ngabbs/post/smile/04.gif',
+            filename: '04.gif',
+            code: '[s:0:24]'
+          },
+          {
+            url: 'http://img4.ngacn.cc/ngabbs/post/smile/15.gif',
+            filename: '15.gif',
+            code: '[s:0:35]'
+          },
+          {
+            url: 'http://img4.ngacn.cc/ngabbs/post/smile/16.gif',
+            filename: '16.gif',
+            code: '[s:0:36]'
+          },
+          {
+            url: 'http://img4.ngacn.cc/ngabbs/post/smile/17.gif',
+            filename: '17.gif',
+            code: '[s:0:37]'
+          },
+          {
+            url: 'http://img4.ngacn.cc/ngabbs/post/smile/18.gif',
+            filename: '18.gif',
+            code: '[s:0:38]'
+          },
+          {
+            url: 'http://img4.ngacn.cc/ngabbs/post/smile/19.gif',
+            filename: '19.gif',
+            code: '[s:0:39]'
+          },
+          {
+            url: 'http://img4.ngacn.cc/ngabbs/post/smile/20.gif',
+            filename: '20.gif',
+            code: '[s:0:40]'
+          },
+          {
+            url: 'http://img4.ngacn.cc/ngabbs/post/smile/21.gif',
+            filename: '21.gif',
+            code: '[s:0:41]'
+          },
+          {
+            url: 'http://img4.ngacn.cc/ngabbs/post/smile/22.gif',
+            filename: '22.gif',
+            code: '[s:0:42]'
+          },
+          {
+            url: 'http://img4.ngacn.cc/ngabbs/post/smile/23.gif',
+            filename: '23.gif',
+            code: '[s:0:43]'
+          }
+        ],
+    },
+    tinyac: {
+      name: '小AC',
+      path: 'tinyac',
+      items: 
+        [
+          {
+            url: "http://img4.ngacn.cc/ngabbs/post/smile/ac0.png",
+            filename: "ac0.png",
+            code: "[s:ac:blink]"
+          },
+          {
+            url: "http://img4.ngacn.cc/ngabbs/post/smile/ac1.png",
+            filename: "ac1.png",
+            code: "[s:ac:goodjob]"
+          },
+          {
+            url: "http://img4.ngacn.cc/ngabbs/post/smile/ac2.png",
+            filename: "ac2.png",
+            code: "[s:ac:上]"
+          },
+          {
+            url: "http://img4.ngacn.cc/ngabbs/post/smile/ac3.png",
+            filename: "ac3.png",
+            code: "[s:ac:中枪]"
+          },
+          {
+            url: "http://img4.ngacn.cc/ngabbs/post/smile/ac4.png",
+            filename: "ac4.png",
+            code: "[s:ac:偷笑]"
+          },
+          {
+            url: "http://img4.ngacn.cc/ngabbs/post/smile/ac5.png",
+            filename: "ac5.png",
+            code: "[s:ac:冷]"
+          },
+          {
+            url: "http://img4.ngacn.cc/ngabbs/post/smile/ac6.png",
+            filename: "ac6.png",
+            code: "[s:ac:凌乱]"
+          },
+          {
+            url: "http://img4.ngacn.cc/ngabbs/post/smile/ac7.png",
+            filename: "ac7.png",
+            code: "[s:ac:反对]"
+          },
+          {
+            url: "http://img4.ngacn.cc/ngabbs/post/smile/ac8.png",
+            filename: "ac8.png",
+            code: "[s:ac:吓]"
+          },
+          {
+            url: "http://img4.ngacn.cc/ngabbs/post/smile/ac9.png",
+            filename: "ac9.png",
+            code: "[s:ac:吻]"
+          },
+          {
+            url: "http://img4.ngacn.cc/ngabbs/post/smile/ac10.png",
+            filename: "ac10.png",
+            code: "[s:ac:呆]"
+          },
+          {
+            url: "http://img4.ngacn.cc/ngabbs/post/smile/ac11.png",
+            filename: "ac11.png",
+            code: "[s:ac:咦]"
+          },
+          {
+            url: "http://img4.ngacn.cc/ngabbs/post/smile/ac12.png",
+            filename: "ac12.png",
+            code: "[s:ac:哦]"
+          },
+          {
+            url: "http://img4.ngacn.cc/ngabbs/post/smile/ac13.png",
+            filename: "ac13.png",
+            code: "[s:ac:哭]"
+          },
+          {
+            url: "http://img4.ngacn.cc/ngabbs/post/smile/ac14.png",
+            filename: "ac14.png",
+            code: "[s:ac:哭1]"
+          },
+          {
+            url: "http://img4.ngacn.cc/ngabbs/post/smile/ac15.png",
+            filename: "ac15.png",
+            code: "[s:ac:哭笑]"
+          },
+          {
+            url: "http://img4.ngacn.cc/ngabbs/post/smile/ac16.png",
+            filename: "ac16.png",
+            code: "[s:ac:哼]"
+          },
+          {
+            url: "http://img4.ngacn.cc/ngabbs/post/smile/ac17.png",
+            filename: "ac17.png",
+            code: "[s:ac:喘]"
+          },
+          {
+            url: "http://img4.ngacn.cc/ngabbs/post/smile/ac18.png",
+            filename: "ac18.png",
+            code: "[s:ac:喷]"
+          },
+          {
+            url: "http://img4.ngacn.cc/ngabbs/post/smile/ac19.png",
+            filename: "ac19.png",
+            code: "[s:ac:嘲笑]"
+          },
+          {
+            url: "http://img4.ngacn.cc/ngabbs/post/smile/ac20.png",
+            filename: "ac20.png",
+            code: "[s:ac:嘲笑1]"
+          },
+          {
+            url: "http://img4.ngacn.cc/ngabbs/post/smile/ac21.png",
+            filename: "ac21.png",
+            code: "[s:ac:囧]"
+          },
+          {
+            url: "http://img4.ngacn.cc/ngabbs/post/smile/ac22.png",
+            filename: "ac22.png",
+            code: "[s:ac:委屈]"
+          },
+          {
+            url: "http://img4.ngacn.cc/ngabbs/post/smile/ac23.png",
+            filename: "ac23.png",
+            code: "[s:ac:心]"
+          },
+          {
+            url: "http://img4.ngacn.cc/ngabbs/post/smile/ac24.png",
+            filename: "ac24.png",
+            code: "[s:ac:忧伤]"
+          },
+          {
+            url: "http://img4.ngacn.cc/ngabbs/post/smile/ac25.png",
+            filename: "ac25.png",
+            code: "[s:ac:怒]"
+          },
+          {
+            url: "http://img4.ngacn.cc/ngabbs/post/smile/ac26.png",
+            filename: "ac26.png",
+            code: "[s:ac:怕]"
+          },
+          {
+            url: "http://img4.ngacn.cc/ngabbs/post/smile/ac27.png",
+            filename: "ac27.png",
+            code: "[s:ac:惊]"
+          },
+          {
+            url: "http://img4.ngacn.cc/ngabbs/post/smile/ac28.png",
+            filename: "ac28.png",
+            code: "[s:ac:愁]"
+          },
+          {
+            url: "http://img4.ngacn.cc/ngabbs/post/smile/ac29.png",
+            filename: "ac29.png",
+            code: "[s:ac:抓狂]"
+          },
+          {
+            url: "http://img4.ngacn.cc/ngabbs/post/smile/ac30.png",
+            filename: "ac30.png",
+            code: "[s:ac:抠鼻]"
+          },
+          {
+            url: "http://img4.ngacn.cc/ngabbs/post/smile/ac31.png",
+            filename: "ac31.png",
+            code: "[s:ac:擦汗]"
+          },
+          {
+            url: "http://img4.ngacn.cc/ngabbs/post/smile/ac32.png",
+            filename: "ac32.png",
+            code: "[s:ac:无语]"
+          },
+          {
+            url: "http://img4.ngacn.cc/ngabbs/post/smile/ac33.png",
+            filename: "ac33.png",
+            code: "[s:ac:晕]"
+          },
+          {
+            url: "http://img4.ngacn.cc/ngabbs/post/smile/ac34.png",
+            filename: "ac34.png",
+            code: "[s:ac:汗]"
+          },
+          {
+            url: "http://img4.ngacn.cc/ngabbs/post/smile/ac35.png",
+            filename: "ac35.png",
+            code: "[s:ac:瞎]"
+          },
+          {
+            url: "http://img4.ngacn.cc/ngabbs/post/smile/ac36.png",
+            filename: "ac36.png",
+            code: "[s:ac:羞]"
+          },
+          {
+            url: "http://img4.ngacn.cc/ngabbs/post/smile/ac37.png",
+            filename: "ac37.png",
+            code: "[s:ac:羡慕]"
+          },
+          {
+            url: "http://img4.ngacn.cc/ngabbs/post/smile/ac38.png",
+            filename: "ac38.png",
+            code: "[s:ac:花痴]"
+          },
+          {
+            url: "http://img4.ngacn.cc/ngabbs/post/smile/ac39.png",
+            filename: "ac39.png",
+            code: "[s:ac:茶]"
+          },
+          {
+            url: "http://img4.ngacn.cc/ngabbs/post/smile/ac40.png",
+            filename: "ac40.png",
+            code: "[s:ac:衰]"
+          },
+          {
+            url: "http://img4.ngacn.cc/ngabbs/post/smile/ac41.png",
+            filename: "ac41.png",
+            code: "[s:ac:计划通]"
+          },
+          {
+            url: "http://img4.ngacn.cc/ngabbs/post/smile/ac42.png",
+            filename: "ac42.png",
+            code: "[s:ac:赞同]"
+          },
+          {
+            url: "http://img4.ngacn.cc/ngabbs/post/smile/ac43.png",
+            filename: "ac43.png",
+            code: "[s:ac:闪光]"
+          },
+          {
+            url: "http://img4.ngacn.cc/ngabbs/post/smile/ac44.png",
+            filename: "ac44.png",
+            code: "[s:ac:黑枪]"
+          }
+        ]
+    }
+  };
+});
